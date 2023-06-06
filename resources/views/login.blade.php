a662d293f3bff979200a38020a25c563b0d1b297
Milan Rubbens <milan.rubbens7@gmail.com>
2023-06-06 11:59:38 +0200

    register login updated

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts\app')
 <section class="vh-100 gradient-custom">
     <div class="container py-5 h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
@@ -8,7 +9,7 @@
               <div class="mb-md-5 mt-md-4 pb-5">
   
                 <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                <p class="text-white-50 mb-5">Please enter your email and password!</p>
   
                 <div class="form-outline form-white mb-4">
                   <input type="email" id="typeEmailX" class="form-control form-control-lg" />
@@ -33,7 +34,7 @@
               </div>
   
               <div>
-                <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+                <p class="mb-0">Don't have an account? <a href="/register" class="text-white-50 fw-bold">Sign Up</a>
                 </p>
               </div>
   
