13b4c5a70ee2690f30f1e2141058e46b2b4f20bb
Milan Rubbens <milan.rubbens7@gmail.com>
2023-06-20 14:14:36 +0200

    changepassword Layout

--- resources/views/changePassword.blade.php
+++ resources/views/changePassword.blade.php
@@ -1,15 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('changePassword') }}">
-    @csrf
-    <label for="current_password">Current Password:</label>
-    <input type="password" name="current_password" id="current_password"><br>
 
-    <label for="new_password">New Password:</label>
-    <input type="password" name="new_password" id="new_password"><br>
-
-    <button type="submit">Change Password</button>
-</form>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Change Password</div>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('changePassword') }}">
+                        @csrf
+                        <label for="current_password">Current Password:</label>
+                        <input type="password" name="current_password" id="current_password"><br>
+                    <br>
+                        <label for="new_password">New Password:</label>
+                        <input type="password" name="new_password" id="new_password"><br>
+                        <button type="submit">Change Password</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
