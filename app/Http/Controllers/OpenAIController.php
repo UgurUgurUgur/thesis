085fbff0b056196015e1fff890fce084f64ca0a3
UgurUgurUgur <ugurkankaraca03@gmail.com>
2023-06-25 01:27:21 +0200

    created controller ai api

--- app/Http/Controllers/OpenAIController.php
+++ app/Http/Controllers/OpenAIController.php
@@ -8,28 +8,20 @@ use App\Models\Todo;
 
 class OpenAIController extends Controller
 {
-    public function handleFormSubmission(Request $request, $taskId)
+    public function viewTask($taskId)
     {
-        // Get the input from the form
-        $userInput = $request->input('user_input');
-    
-        // Get the description from the database based on the task ID
         $task = Todo::findOrFail($taskId);
-        $description = $task->description;
+        
+        $prompt = "How can I do this task the most efficient way? This is the description of my task: " . $task->description;
     
-        // Combine the user input and description as the prompt
-        $prompt = "How can I do this task the most efficient way? This is the description of my task: " . $description;
-    
-        // Send the prompt to the ChatGPT API
         $response = Http::post('https://api.openai.com/v1/engines/davinci-codex/completions', [
             'prompt' => $prompt,
             'max_tokens' => 100,
         ])->throw();
     
-        // Get the generated response from the API
         $chatGptResponse = $response['choices'][0]['text'];
     
-        return view('viewTasks', compact('chatGptResponse', 'task'));
+        return view('viewTasks', compact('task', 'chatGptResponse'));
     }
-
+    
 }
